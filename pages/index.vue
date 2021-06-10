a1358b0d49297aa2b49f998777538c5333f25063
GrahamSH <grahamshllk@gmail.com>
2021-06-10 15:19:19 +0000

    👷 Add .gitpod.yml

--- pages/index.vue
+++ pages/index.vue
@@ -125,9 +125,8 @@ export default {
           return 'bg-green-400 text-white'
         case 'purple':
           return 'bg-purple-400 text-white'
-
         default:
-          return 'bg-red-400 text-white'
+          return ''
       }
     },
   },
