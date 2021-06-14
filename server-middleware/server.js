de8056733faf9ffb2b66ca2607822aba6fe1b984
GrahamSH <grahamshllk@gmail.com>
2021-06-14 15:46:38 +0000

    🐛 Remove autodelete

--- server-middleware/server.js
+++ server-middleware/server.js
@@ -3,7 +3,6 @@ var crypto = require('crypto')
 const bodyParser = require('body-parser')
 
 const db = require('monk')(process.env.URL)
-db.log_events.ensureIndex({ "createdAt": 1 }, { expireAfterSeconds: 2592000 })
 
 const assignments = db.get('assignments')
 console.log(assignments)
@@ -11,19 +10,20 @@ app.use(bodyParser.json())
 app.post('/get', (req, res) => {
   if (req.body.assignments && req.body.subject) {
     id = crypto.randomBytes(4).toString('hex')
-    assignments.insert({subject: req.body.subject, assignments: req.body.assignments, id})
+    assignments.insert({
+      subject: req.body.subject,
+      assignments: req.body.assignments,
+      id,
+    })
     res.send(id)
   } else {
     res.sendStatus(400).send('Missing data')
   }
-  
 })
 app.get('/add/:str', async (req, res) => {
-  data = await assignments.findOne({ id: req.params.str });
+  data = await assignments.findOne({ id: req.params.str })
   console.log(data)
-  res.redirect(
-    `/#${JSON.stringify(data)}`
-  )
+  res.redirect(`/#${JSON.stringify(data)}`)
 })
 
 module.exports = app
