226908a118439efe3d856ae435cf941b8d9ad08b
GrahamSH <grahamshllk@gmail.com>
2021-07-22 19:22:31 +0000

    Blocks Coming Soon

--- layouts/default.vue
+++ layouts/default.vue
@@ -63,35 +63,164 @@
       </div>
     </div>
 
-    <div class=" p-1 flex justify-between">
-      <div class="font-serif font-bold text-4xl"><img
-        class="rounded-full h-10 w-10 inline align-text-bottom"
-        src="~/static/logo.svg"
-      />
-      <nuxt-link to="/">E-jenda</nuxt-link> <span class="text-base">v{{ version }}</span></div>
-          <nuxt-link to="/faq" class="bg-red-300 text-red-100 hover:bg-red-700 hover:text-white px-3 py-2 rounded-md">FAQ</nuxt-link>
-
+    <div class="p-1 flex justify-between">
+      <div class="font-serif font-bold text-4xl">
+        <img
+          class="rounded-full h-10 w-10 inline align-text-bottom"
+          src="~/static/logo.svg"
+        />
+        <nuxt-link to="/">E-jenda</nuxt-link>
+        <span class="text-base">v{{ version }}</span>
+      </div>
+      <nuxt-link
+        to="/faq"
+        class="
+          bg-red-300
+          text-red-100
+          hover:bg-red-700 hover:text-white
+          px-3
+          py-2
+          rounded-md
+        "
+        >FAQ</nuxt-link
+      >
     </div>
 
     <Nuxt />
     <footer class="bg-red-50 p-4">
       <span class="text-1xl text-red-600 font-bold p-4">© 2021 GrahamSH</span>
     </footer>
+    <div class="inset-x-0 bottom-0 rounded sticky" v-show="!hide">
+      <div
+        class="
+          max-w-7xl
+          mx-auto
+          py-3
+          px-3
+          sm:px-6
+          lg:px-8
+          m-2
+          bg-red-600
+          rounded
+        "
+      >
+        <div class="flex items-center justify-between flex-wrap">
+          <div class="w-0 flex-1 flex items-center">
+            <span class="flex p-2 rounded-lg bg-red-800">
+              <!-- Heroicon name: outline/speakerphone -->
+              <svg
+                class="h-6 w-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
+                />
+              </svg>
+            </span>
+            <p class="ml-3 font-medium text-white truncate">
+              <span class="">
+                Coming soon: Ejenda Blocks
+              </span>
+            </p>
+          </div>
+          <div
+            class="
+              order-3
+              mt-2
+              flex-shrink-0
+              w-full
+              sm:order-2 sm:mt-0 sm:w-auto
+            "
+          >
+            <nuxt-link
+              to="/blocks"
+              class="
+                flex
+                items-center
+                justify-center
+                px-4
+                py-2
+                border border-transparent
+                rounded-md
+                shadow-sm
+                text-sm
+                font-medium
+                text-red-600
+                bg-white
+                hover:bg-red-50
+                
+              "
+            >
+              Learn more
+            </nuxt-link>
+          </div>
+          <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+            <button
+              type="button"
+              class="
+                -mr-1
+                flex
+                p-2
+                rounded-md
+                hover:bg-red-500
+                focus:outline-none focus:ring-2 focus:ring-white
+                sm:-mr-2
+              "
+              @click="hideComingSoonPopup"
+            >
+              <span class="sr-only">Dismiss</span>
+              <!-- Heroicon name: outline/x -->
+              <svg
+                class="h-6 w-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { version } from '~/package.json'
 export default {
   data() {
-    return { version }
+    return { version, hide: true }
+  },
+  mounted() {
+    this.hide = localStorage.getItem('hide') ?? false
+  },
+  methods: {
+    hideComingSoonPopup() {
+      this.hide = true
+      localStorage.setItem('hide', 'true')
+    }
   },
 }
 </script>
 <style lang="postcss">
 .mx-input {
   @apply !h-[40px];
-  border: none!important;
-  box-shadow: none!important;
+  border: none !important;
+  box-shadow: none !important;
 }
 .link {
   @apply text-red-400;
---
+++ pages/blocks.vue
@@ -0,0 +1,145 @@
+<template>
+  <div
+    class="relative max-w-7xl px-4 min-h-screen sm:px-6 lg:px-8 mx-auto lg:flex"
+  >
+    <div
+      class="
+        absolute
+        -inset-y-20
+        left-2
+        sm:left-4
+        xl:left-2
+        w-px
+        bg-gray-900/[0.08]
+      "
+    ></div>
+    <div
+      class="
+        absolute
+        -inset-y-20
+        right-2
+        sm:right-4
+        xl:right-2
+        w-px
+        bg-gray-900/[0.08]
+      "
+    ></div>
+
+    <div class="flex-none max-w-screen-sm lg:max-w-xl mx-auto lg:ml-0 lg:mr-16">
+      <main class="pb-16 lg:pb-20">
+        <p
+          class="
+            font-semibold
+            text-light-red-100
+            leading-relaxed
+            uppercase
+            tracking-wider
+          "
+        >
+          Coming soon
+        </p>
+        <h1
+          class="
+            mt-4
+            text-4xl
+            leading-none
+            sm:text-5xl
+            font-extrabold
+            tracking-tight
+          "
+        >
+          Talk, post resources, and see upcoming events <span class="text-red-600">with your class</span>
+        </h1>
+
+        <p class="mt-5 text-gray-800  leading-relaxed">
+            Ejenda Blocks lets you chat with your class, share Ejenda assignments, and post resources for the rest of your class to use.
+        </p>
+        <div class="mt-8">
+          <h2 class="font-semibold leading-relaxed mb-3 flex items-center">
+            <span class="sm:hidden">Get notified when it’s ready</span>
+            <span class="hidden sm:inline"
+              >Get behind-the-scenes updates and find out when it’s ready</span
+            >
+            <svg
+              width="12"
+              height="12"
+              fill="currentColor"
+              class="ml-2 relative top-px"
+            >
+              <path
+                d="M6.182 11.023l5.511-5.512L6.182 0l-1.17 1.165 3.505 3.5H0v1.693h8.517L5.011 9.864l1.17 1.159z"
+              ></path>
+            </svg>
+          </h2>
+          <div
+            class="sm:flex"
+          >
+   
+            <a
+              type="submit"
+              class="
+                appearance-none
+                w-full
+                sm:w-auto sm:flex-none
+                mt-3
+                sm:mt-0 
+                rounded-lg
+                px-6
+                h-12
+                font-semibold
+                flex
+                items-center
+                justify-center
+                focus:outline-none focus:ring-4
+                text-white
+                shadow-sm
+                bg-red-900
+                hover:bg-red-800
+                focus:ring-white/40
+              "
+              href="https://grahamsh.com/contact"
+            >
+              Get notified
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+    <div
+      class="
+        flex-none
+        w-[32.75rem]
+        h-[32.75rem]
+        mx-auto
+        lg:mr-0
+        relative
+        rounded-t-lg
+        overflow-hidden
+        shadow-2xl
+      "
+    >
+      <svg
+        viewBox="0 0 500 500"
+        class="lg:absolute lg:top-0 lg:left-0 lg:w-full lg:h-auto"
+      >
+        <defs>
+          <radialGradient id="gradient">
+            <stop offset="40%" stop-color="white"></stop>
+            <stop offset="94%" stop-color="white" stop-opacity="0"></stop>
+          </radialGradient>
+          <mask id="mask">
+            <circle cx="105" cy="0" r="1300" fill="url(#gradient)"></circle>
+          </mask>
+        </defs>
+        <g mask="url(#mask)">
+          <rect width="500" height="500" fill="white"></rect>
+          <image
+            xlink:href="/logo.svg"
+            width="500"
+            height="500"
+          ></image>
+        </g>
+      </svg>
+    </div>
+  </div>
+</template>
