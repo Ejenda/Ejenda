06801ee7716c3005f70c419986bc5fd4a79ff97c
Thingyman-DEV <njsiegel9@gmail.com>
2021-10-14 13:19:58 +0000

    💄 Another UI update, big surprise from Mr. UI

--- components/Tabs.vue
+++ components/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-2 flex dark:bg-gray-900 shadow-lg ">
-    <nuxt-link class="dark:text-red-200 mx-2" to="/app">List</nuxt-link>
-    <nuxt-link class="dark:text-red-200 mx-2" to="/app/calendar">Calendar</nuxt-link>
+    <nuxt-link class="dark:text-red-200 border-b-2 dark:border-gray-900 dark:hover:border-gray-600 transition border-white hover:border-gray-500 mx-2" to="/app">List</nuxt-link>
+    <nuxt-link class="dark:text-red-200 border-b-2 dark:border-gray-900 dark:hover:border-gray-600 transition border-white hover:border-gray-500 mx-2" to="/app/calendar">Calendar</nuxt-link>
 </div>
 </template>
 <style lang="postcss" scoped>
