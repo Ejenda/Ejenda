c5c919597b9928c27d9772df857ddcd3009b74fa
GrahamSH <grahamshllk@gmail.com>
2021-11-02 15:52:51 +0000

    💄 Add dark variant for todo page

--- components/Todo.vue
+++ components/Todo.vue
@@ -1,5 +1,5 @@
 <template>
-  <details class="bg-red-400 rounded-md p-2 m-2">
+  <details class="bg-red-400 dark:bg-gray-400 rounded-md p-2 m-2">
     <summary class="p-2 m-2 font-bold cursor-pointer">{{ name }}</summary>
     <ul>
       <li v-for="assignment of assignments" :key="assignment.id" class="
