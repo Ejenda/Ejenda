a5e653c28a4ef81446db941651252737734fd697
GrahamSH <grahamshllk@gmail.com>
2022-09-22 15:39:17 +0000

    Revert "Alow for asyncronous instances"
    This reverts commit e640b2e9e4aefabd4c95e1f7f905effaab1b6f89.

--- server-middleware/server.js
+++ server-middleware/server.js
@@ -74,16 +74,14 @@ let sessions = [];
 })();
 
 const strictCors = {};
-async function findSession(token) {
-
-  const session = (await Session.findOne({token}).exec());
-  console.log(session)
+function findSession(token) {
+  const session = sessions.find((f) => f.token == token);
   return session;
 }
 async function addSession(token, id, time) {
   // defaults to 6 hours
   new Session({ id: id, token: token }).save();
-  //sessions.push({ id: id, token: token });
+  sessions.push({ id: id, token: token });
 
   if (time) {
     setTimeout(() => {
@@ -95,10 +93,12 @@ async function addSession(token, id, time) {
 
 async function removeSession(token) {
   await Session.deleteOne({ token });
+  sessions = sessions.filter((obj) => {
+    return obj.token !== token;
+  });
 }
 
 function findUser(id) {
-  console.log(id)
   id = id.toString();
   if (id.length !== 24) {
     id = "000000000000000000000001";
@@ -117,9 +117,8 @@ function findUser(id) {
 app.use(async (req, res, next) => {
   // get user
   const token = req.headers.authorization || req.cookies.token;
-  let user = await findSession(token);
+  let user = findSession(token);
   if (user) {
-    console.log('user', user)
     res.locals.requester = await findUser(user.id);
     if (res.locals.requester) {
       res.locals.loggedIn = true;
