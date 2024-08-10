77fdcc3f2c846f54c1dcc519b2a1281906934fee
GrahamSH <grahamshllk@gmail.com>
2024-08-10 10:47:53 -0400

    make things More Usable :tm:

---
+++ composables/mutations/createSubject.ts
@@ -0,0 +1,43 @@
+///import KSUID from "ksuid";
+
+import { date } from "drizzle-orm/mysql-core";
+
+export default function () {
+    const queryClient = vq.useQueryClient();
+    return vq.useMutation({
+        mutationKey: ["createSubject"],
+        mutationFn: async (newSubject: Object) => {
+            console.log(newSubject);
+            const data = await $fetch("/api/subjects/create", {
+                method: "POST",
+                body: JSON.stringify(newSubject),
+            });
+            return data;
+        },
+        // optimisticly update local state, allowing for rollback on error
+        onMutate: async (newSubject: any) => {
+            await queryClient.cancelQueries({ queryKey: ["assignments"] });
+            // Snapshot the previous value
+            const previousSubjects = queryClient.getQueryData(["assignments"]);
+            // Optimistically update to the new value
+            queryClient.setQueryData(["assignments"], (old: any) => {
+              console.log(old);
+              old.push({
+                id: `subj_${Date.now()}`,
+                name: newSubject.name,
+                assignments: [],
+              });
+              return old;
+            });
+            // Return a context object with the snapshotted value
+            return { previousSubjects };
+        },
+        onError: async (error, variables, context) => {
+            // Rollback on error
+            queryClient.setQueryData(["assignments"], context?.previousSubjects);
+        },
+        onSettled: async () => {
+            await queryClient.invalidateQueries({ queryKey: ["assignments"] });
+        },
+    });
+}
--- pages/app/index.vue
+++ pages/app/index.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { titleCase } from "scule";
+import { format } from "date-fns";
+
 definePageMeta({
   middleware: "auth",
 });
 const { data, isPending } = queryAssignments();
+const { mutate,isError } = addAssignment();
+const { mutate: createSubjectMut } = createSubject();
+
 const tabsLinks = [
   { label: "Subjects", to: "/app", icon: "i-heroicons-academic-cap" },
   { label: "Todo", to: "/app/todo", icon: "i-heroicons-check-circle" },
@@ -11,27 +16,42 @@ const tabsLinks = [
 ];
 const currentSubjectId = ref(data.value?.[0].id);
 const links = computed(() => {
-  return data.value?.map((subject) => {
-    const done = subject.assignments.filter(
-      (assignment) => !assignment.tags?.includes("done")
-    ).length;
-    const missing = subject.assignments.some(
-      (assignment) =>
-        !assignment.due || done || new Date(assignment.due) < new Date()
-    );
-    return {
-      label: titleCase(subject.name, { normalize: true }),
+  let subjectLinks =
+    data.value?.map((subject) => {
+      const done = subject.assignments.filter(
+        (assignment) => !assignment.tags?.includes("done")
+      ).length;
+      const missing = subject.assignments.some(
+        (assignment) =>
+          !assignment.due || done || new Date(assignment.due) < new Date()
+      );
+      return {
+        label: titleCase(subject.name, { normalize: true }),
+        click: () => {
+          currentSubjectId.value = subject.id;
+        },
+        active: subject.id === currentSubjectId.value,
+        badge: {
+          label: done,
+          variant: missing ? "subtle" : "soft",
+          color: missing ? "red" : "white",
+        },
+      };
+    }) ?? [];
+
+  return [
+    {
+      label: "New subject",
+      icon: "i-heroicons-plus-circle",
       click: () => {
-        currentSubjectId.value = subject.id;
+        const name = prompt("Enter subject name");
+        if (name) {
+          createSubjectMut({ name });
+        }
       },
-      active: subject.id === currentSubjectId.value,
-      badge: {
-        label: done,
-        variant: missing ? "subtle" : "soft",
-        color: missing ? "red" : "white",
-      },
-    };
-  });
+    },
+    ...subjectLinks,
+  ];
 });
 const currentSubject = computed(() =>
   data.value?.find((subject) => subject.id === currentSubjectId.value)
@@ -45,7 +65,18 @@ const assnWithSubject = computed(() => ({
   ...assn.value,
   subjectId: currentSubjectId.value,
 }));
-const { mutate } = addAssignment();
+const createAssn = () => {
+  mutate(assnWithSubject.value);
+  if (!isError.value) {
+    assn.value = {
+      name: "",
+      tags: [],
+      due: new Date(),
+    };
+  }
+}
+  
+        
 </script>
 <template>
   <UHorizontalNavigation :links="tabsLinks"></UHorizontalNavigation>
@@ -56,8 +87,17 @@ const { mutate } = addAssignment();
       <div>
         <!-- create new assn-->
         <UInput v-model="assn.name" placeholder="Assignment name" />
+        <UPopover :popper="{ placement: 'bottom-start' }">
+          <UButton
+            icon="i-heroicons-calendar-days-20-solid"
+            :label="format(assn.due, 'd MMM, yyy')"
+          />
 
-        <div @click="mutate(assnWithSubject)">Create new assignment</div>
+          <template #panel="{ close }">
+            <DatePicker v-model="assn.due" is-required @close="close" :min-date="new Date()"/>
+          </template>
+        </UPopover>
+        <UButton @click="createAssn">Create new assignment</UButton>
       </div>
       <div
         v-for="assignment in currentSubject?.assignments"
@@ -65,6 +105,7 @@ const { mutate } = addAssignment();
         class="p-4 border rounded-md"
       >
         <h2>{{ assignment.title }}</h2>
+        <p v-if="assignment.due">Due: {{  format(new Date(assignment.due), 'd MMM, yyy') }}</p>
         <div v-for="tag of assignment.tags" :key="tag">
           <span>{{ tag }}</span>
         </div>
