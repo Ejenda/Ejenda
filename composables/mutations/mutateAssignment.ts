6463eb78ddecbfbcda7309898a4dccda3b6136ce
GrahamSH <grahamshllk@gmail.com>
2024-08-15 13:19:51 -0400

    in place update on mutate assignment

--- composables/mutations/mutateAssignment.ts
+++ composables/mutations/mutateAssignment.ts
@@ -12,6 +12,33 @@ export default function () {
         },
 
         // optimisticly update local state, allowing for rollback on error
+        onMutate: async (newAssignment: any) => {
+            await queryClient.cancelQueries({ queryKey: ["assignments"] });
+
+            // Snapshot the previous value
+            const previousAssignments = queryClient.getQueryData(["assignments"]);
+
+            // Optimistically update to the new value
+            queryClient.setQueryData(["assignments"], (old: any) => {
+                const subjectId = newAssignment.subjectId;
+                const subject = old.find((x: any) => x.id == subjectId)
+                subject.assignments = subject.assignments.filter(
+                  (x: any) => x.id !== newAssignment.id
+                );
+                subject.assignments.push(newAssignment);
+                
+                return old;
+            });
+
+
+            // Return a context object with the snapshotted value
+            return { previousAssignments };
+        },
+        onError: async (error, variables, context) => {
+            // Rollback on error
+            $queryClient.setQueryData(["assignments"], context.previousAssignments);
+        },
+
         onSettled: async () => {
             await queryClient.invalidateQueries({ queryKey: ["assignments"] });
         },
